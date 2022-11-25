08e1a1b256f39c7f8f1a5f65848ce6bd8a2eff7f
Stranger <45332516+Makemethink@users.noreply.github.com>
2022-11-26 00:40:20 +0530

    main.js v2.0
    Adding Editing functionality and resolving max character bug

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const inbox = document.getElementById('inbox');
 const addbtn= document.getElementById('addbtn');
 const clrbtn= document.getElementById('clrbtn');
 const todo = document.querySelector('.todolist');
+let indexed = 0;
 showtask();
 inbox.onkeyup = ()=>{
     let userData = inbox.value;
@@ -30,6 +31,7 @@ addbtn.onclick = ()=>{
      listarr = JSON.parse(getlocal);
    }
    listarr.push(userData);
+
    localStorage.setItem('New Todo', JSON.stringify(listarr));
    showtask();
 }
@@ -44,7 +46,8 @@ function showtask(){
   }
   let newlitag = '';
   listarr.forEach((item, i) => {
-    newlitag += '<li>' + item + '<span onclick="deleteit('+ i +')">-</span> </li>';
+    //newlitag += '<li>' + item + '<span onclick="openit('+ i +')">+<span onclick="deleteit('+ i +')" >-</span></span></li>';
+    newlitag += '<li>' + item + '<div class="span" style="display:inline" onclick="openit('+ i +')">+</div><div class="spans" style="display:inline" onclick="deleteit('+i+')">-</div></li>';
     todo.innerHTML = newlitag;
     inbox.value = "";
     addbtn.classList.remove("active");
@@ -53,7 +56,35 @@ function showtask(){
   });
 
 }
+function save(){
+  alert("saving it...");
+  let getlocal = localStorage.getItem("New Todo");
+  let edit = document.getElementById('exampleModalLongTitle');
+  listarr = JSON.parse(getlocal);
+  listarr[indexed] = edit.innerHTML;
+  $('#exampleModalCenter').modal('hide');
+  localStorage.setItem('New Todo', JSON.stringify(listarr));
+  showtask();
+}
+function openit(index){
+  let getlocal = localStorage.getItem("New Todo");
+  listarr = JSON.parse(getlocal);
+  $('#exampleModalCenter').modal('show');
+  let edit = document.getElementById('exampleModalLongTitle');
+  edit.innerHTML = listarr[index];
+  indexed = index;
+}
+
 function deleteit(index){
+ if (confirm("You want to delete it!")) {
+   txt = 1;
+   } else {
+     txt = 2;
+   }
+   if(txt == 2)
+   {
+     return;
+   }
   let getlocal = localStorage.getItem("New Todo");
   listarr = JSON.parse(getlocal);
   listarr.splice(index, 1);
